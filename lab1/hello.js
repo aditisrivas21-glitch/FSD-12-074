dfd41be41e419abfcb1368d872823fc1d56319cc
Aditi Srivastava <aditisrivas21@gmail.com>
2026-07-23 11:53:52 +0530

    initial commit

---
+++ lab1/hello.js
@@ -0,0 +1,15 @@
+console.log("Ananya rai");
+// write a function to take two number and return
+//their sum
+function sum(a, b) {
+  return a + b;
+}
+const add = (a, b) => {
+  //arrow function
+  return a + b;
+};
+const add2 = (a, b) => a + b;
+
+console.log(sum(10, 10));
+console.log(add(10, 10));
+console.log(add2(10, 10));
