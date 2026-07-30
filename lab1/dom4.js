0b96435a2a7bb21d28145d82e8506e6c4ebc8b12
Aditi Srivastava <aditisrivas21@gmail.com>
2026-07-30 11:44:23 +0530

    final lab1

--- lab1/dom4.js
+++ lab1/dom4.js
@@ -1,6 +1,11 @@
-import { EventEmitter } from 'events';
+import { EventEmitter } from "events";
 
 const form = new EventEmitter();
-form.on("submit",(uname,password)=>{
-    console.log('form Submitted')
-}
+
+form.on("submit", (uname, password) => {
+  console.log("Form Submitted");
+  console.log(`User name: ${uname}`);
+  console.log(`User password: ${password}`);
+});
+
+form.emit("submit", "abc@abc.com", "123344");
---
+++ lab1/dom5.js
@@ -0,0 +1,29 @@
+import { EventEmitter } from 'events';
+
+class DomClass extends EventEmitter{
+    addEventListener(eventName, callback) {
+        this.on(eventName, callback);
+    }
+
+    removEventListner(eventName, callback){
+        this.off(eventName, callback);
+    }
+dispatchEvent(eventName,eventData={}){
+    const event = { 
+        type: eventName,
+        timestamp: new Date(),
+        ...eventData,
+    };
+    this.emit(eventName, event)
+}
+}
+const button = new DomClass();
+const hanleClick = (event) => {
+    console.log(`Button clicked type: ${event.type} at ${event.timestamp}`);
+};
+button.addEventListener("click", handleClick);
+button.dispatchEvent("click",{
+  target:"resetBtn",
+});
+
+
