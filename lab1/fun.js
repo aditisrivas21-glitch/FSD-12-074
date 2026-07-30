644ab324795d206dede3c2a48b855bacc0472bfd
Aditi Srivastava <aditisrivas21@gmail.com>
2026-07-30 11:12:14 +0530

    dom start

--- lab1/fun.js
+++ lab1/fun.js
@@ -3,3 +3,10 @@
 //0-one
 //1-two
 
+const toWords = (digit)=>{
+    const words=["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"];
+    return words[digit];
+}
+console.log(toWords(5));
+console.log(toWords(3));
+console.log(toWords(6));
