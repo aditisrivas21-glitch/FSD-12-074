d0fd6312c1f50d8589be4d3e7bd37001f4ee0ff5
Aditi Srivastava <aditisrivas21@gmail.com>
2026-08-13 11:47:38 +0530

    file2 deletefile

---
+++ lab2/file2.js
@@ -0,0 +1,55 @@
+import{readFile,appendFile,writeFile} from "fs/promises";
+ 
+
+
+const readData =async(filename)=> 
+    {
+        try{
+        const content=await readFile(filename, "utf-8");
+        
+        return content;
+    } catch(e) {
+        console.log(e.message);
+        console.log("File not found");
+    }
+    finally{
+        console.log("Read data finished");
+    }
+};
+
+
+const writeData =async(filename, content)=>
+    {
+         try {
+            await writeFile(filename, content);
+         } catch (error) {
+            console.log(error.message);
+            
+         }
+         
+};
+
+const appendData =async(filename, content)=>
+    {
+   try {
+     await appendFile(filename, content);
+   } catch (error) {
+     console.log(error.message);
+     
+   }
+         
+};
+
+const deleteFile =async(filename)=>
+{
+    try {
+        await unlink(filename);
+    } catch (error) {
+        console.log("File not found");
+        
+    }
+};
+const data=await readData("file1.js");
+console.log(data);
+
+//if a fun uses awake keyword then the function must be async 
