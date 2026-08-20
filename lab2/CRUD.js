5ecb28391a8284b3eb95556bb5f0fa1cfd59b931
Aditi Srivastava <aditisrivas21@gmail.com>
2026-08-20 10:50:28 +0530

    crud done

--- lab2/CRUD.js
+++ lab2/CRUD.js
@@ -2,29 +2,29 @@ import readline from "readline/promises";
 import { stdin, stdout } from "process";
 import { readFile, writeFile } from "fs/promises";
 
-const FILE = "./cart.json";
+const FILE = "product.json";
 
 const getCart = async () => {
   const data = await readFile(FILE, "utf-8");
   return JSON.parse(data);
 };
 
-const saveCart = async (mycart) => {
-  await writeFile(FILE, JSON.stringify(mycart, null, 2));
+const saveCart = async (myCart) => {
+  await writeFile(FILE, JSON.stringify(myCart, null, 2));
 };
-
 
 const addToCart = async (product) => {
-    const mycart = await getCart();
-    const isfound = mycart.find((item) => item.id === product.id);
-    if(isfound){
-        isfound.qty += product.qty;
-    } else {
-        mycart.push(product);
-    }
-    await saveCart(mycart);
-    console.log(`product updated/added with id ${product.id} into cart`);
+  const myCart = await getCart();
+  const isFound = myCart.find((item) => item.id === product.id);
+  if (isFound) {
+    isFound.qty += product.qty;
+  } else {
+    myCart.push(product);
+  }
+  await saveCart(myCart);
+  console.log(`product added/updated with id ${product.id} into cart`);
 };
+
 const showCart = async () => {
   const data = await getCart();
   console.table(data);
@@ -32,33 +32,24 @@ const showCart = async () => {
 
 const main = async () => {
   let choice;
-
-  const cin = readline.createInterface({
-    input: stdin,
-    output: stdout,
-  });
-
+  const cin = readline.createInterface({ input: stdin, output: stdout });
   do {
-    console.log("\nWelcome to Flipkart 🛍️");
-    console.log("1........Show cart");
-    console.log("2........Add to cart");
-    console.log("3........Remove Product");
-    console.log("4........Update Product");
-    console.log("5........Exit");
-
-    choice = await cin.question("Enter your choice: ");
-
+    console.log("Welcome to Flipkart 🤸");
+    console.log("1.......... Show cart");
+    console.log("2.......... Add Product");
+    console.log("3.......... Remove Product");
+    console.log("4.......... Update Quantity");
+    console.log("5.......... Checkout");
+    choice = await cin.question("Enter your choice:");
     switch (Number(choice)) {
       case 1:
         await showCart();
         break;
-
       case 2:
-        let data= await cin.question("Enter id,anem,price,qty:");
-        const [id,name,price,qty] =
-        data.split(',').map((item) => item.trim());
-
-
+        let data = await cin.question("Enter id,name,price,qty:");
+        const [id, name, price, qty] = data
+          .split(",")
+          .map((item) => item.trim());
         const product = {
           id: Number(id),
           name,
@@ -66,25 +57,21 @@ const main = async () => {
           qty: Number(qty),
         };
         await addToCart(product);
-        break;
 
+        break;
       case 3:
-        console.log("Remove Product");
+        console.log("remove product");
         break;
-
       case 4:
-        console.log("Update Product Quality");
+        console.log("Update product quantity");
         break;
-
       case 5:
         console.log("See you later");
         break;
-
       default:
-        console.log("Invalid Choice! Try again 😒");
+        console.log("Invalid choice! try again 🛑");
     }
-  } while (Number(choice) !== 5);
-
+  } while (choice != 5);
   cin.close();
 };
 
